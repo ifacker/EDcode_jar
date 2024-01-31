1c5912c72e7e7113ca578a7fa02b52acae9f0e8c
ifacker <2677863724@qq.com>
2024-01-31 11:37:16 +0800

    update v1.0.1

--- src/main/java/plugins/EDcode/Config/GlobalConfig.java
+++ src/main/java/plugins/EDcode/Config/GlobalConfig.java
@@ -3,7 +3,7 @@ package plugins.EDcode.Config;
 import plugins.EDcode.DataType.Config;
 
 public class GlobalConfig {
-    public static String VERSION = "1.0";   // 插件版本号
+    public static String VERSION = "1.0.1";   // 插件版本号
     public static Config CONFIG;    // 在内存中的配置
     public static String CONFIG_PATH = "plugins/EDcode/config/config.json";   // 配置文件保存路径
 
